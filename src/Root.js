61d1506ac06e4e14865efd65eea04393ab864a26
ridhomujizat <ridhomujizat@gmail.com>
2021-04-10 22:04:56 +0700

    Push notification

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,20 @@ import React, { Component } from 'react';
 import io from './helpers/socket';
 import { connect } from 'react-redux';
 import { chatRoom, chatList } from './redux/actions/chat';
+import PushNotification from 'react-native-push-notification'
+
+PushNotification.createChannel(
+  {
+    channelId: 'chat', // (required)
+    channelName: 'New Chat', // (required)
+    channelDescription: 'A channel to categorise your notifications',
+    playSound: false,
+    soundName: 'default',
+    importance: 4,
+    vibrate: true
+  },
+  (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+)
 
 class Root extends Component {
   state = {
@@ -19,13 +33,13 @@ class Root extends Component {
     const { token } = this.props.auth
     const { idReceiver } = this.props.chat
     if (token) {
-      io.on(id, (msg) => {
-        this.getListChat(token)
-      })
-    }
-    if (idReceiver) {
       io.on(id, async (msg) => {
-        await this.getRoomChat(token, idReceiver)
+        await this.getListChat(token)
+        PushNotification.localNotification({
+          channelId: 'chat',
+          title: 'New Message',
+          message: msg
+        })
       })
     }
   }
